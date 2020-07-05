93687ba3d77a6e3df110368b2f68eb2799d2645f
yinonhever <yinonehever@gmail.com>
2020-07-05 16:47:01 +0300

    update

---
+++ src/components/Listing.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Listing = props => (
+    <div className="listing">
+        <img src={props.logo} alt="logo" />
+    </div>
+)
+
+export default Listing;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,41 @@
 import React, { useState } from "react";
 import listings from "../data";
-import "../images/account.svg";
+import Listing from "./Listing";
 
 const Main = () => {
     const [filteredListings, setFilteredListings] = useState(listings);
-    const [filters, setFilters] = useState([]);
+    const [filters, setFilters] = useState(["Sass", "Javascript"]);
 
-    console.log(listings);
+    const handleFilter = selection => {
+        setFilters(filters => [...filters, selection]);
+    }
+
+    const handleRemoveFilter = selection => {
+        setFilters(filters => filters.filter(item => item !== selection));
+    }
+
+    const listingMatchingFilters = (item, selections) => {
+        const combined = [...item.languages, ...item.tools];
+        console.log(combined);
+        console.log(selections);
+        return arrayContainsArray(combined, selections);
+    }
+
+    function arrayContainsArray (superset, subset) {
+        return subset.every(function (value) {
+          return (superset.indexOf(value) >= 0);
+        });
+      }
+
+
+      const combined = [...listings[4].languages, ...listings[4].tools];
+      console.log(combined);
+      console.log(filters);
+    console.log(arrayContainsArray(combined, filters));
 
     return (
         <main className="main">
-            <img src="../images/account.svg" alt="icon"></img>
+
         </main>
     )
 }
